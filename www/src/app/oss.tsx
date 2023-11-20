f90ec99b89bdaf9eb661053748ed945cb311f626
Victor Ndaba <mwangindaba@gmail.com>
2023-11-20 03:27:57 +0300

    chore: oss projects updates

--- www/src/app/oss.tsx
+++ www/src/app/oss.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 export function JumbaOpenSource() {
   const oss = [
     {
-      title: "Amplify Access Analzer",
+      title: "Lambda Utils",
+      description:
+        "A collection of utilities for working with AWS Lambda functions. Useful when working with streams from DynamoDB and performing AppsSync operations.",
+      href: "https://www.npmjs.com/package/@jumba/lambda-utils",
+    },
+    {
+      title: "Amplify Access Analyzer",
       description:
         "A package to help you automate the creation of an Access Analyzer for dyamically testing your AppSync APIs security",
       href: "https://www.npmjs.com/package/@jumba/access-analyzer",
@@ -20,9 +26,9 @@ export function JumbaOpenSource() {
     <div className="p-10 mx-auto max-w-6xl">
       <h3 className="text-4xl font-medium mb-5">Jumba Open Source</h3>
       <p className="text-xl max-w-3xl text-gray-500">
-          We use a lot of open source software at Jumba and we believe in
-          contributing back to the community as much as we can. Some of the 
-          projects we have created or have contributed to are listed below.
+        We use a lot of open source software at Jumba and we believe in
+        contributing back to the community as much as we can. Some of the
+        projects we have created or have contributed to are listed below.
       </p>
       <section className="my-14 grid grid-cols-1 md:grid-cols-3 gap-10">
         {oss.map((project) => (
