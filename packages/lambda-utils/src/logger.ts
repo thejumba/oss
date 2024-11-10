6aad01034293028c84617558e452f117cf979d7b
Victor Ndaba <mwangindaba@gmail.com>
2024-11-10 19:03:35 +0300

    refactor: include sentry `traceId` in log object

--- packages/lambda-utils/src/logger.ts
+++ packages/lambda-utils/src/logger.ts
@@ -1,7 +1,13 @@
+import { getActiveSpan } from "@sentry/aws-serverless";
 import { pino } from "pino";
 
 export const logger = pino({
   base: {
     environment: process.env.ENV,
+  },
+  mixin() {
+    return {
+      traceId: getActiveSpan()?.spanContext().traceId,
+    };
   },
 });
